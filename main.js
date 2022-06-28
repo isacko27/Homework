4908cca983e7f2ed04753f6ed0461996413e255d
isacko27 <isaachervoso1@gmail.com>
2022-06-27 18:10:10 -0600

    Homework

---
+++ main.js
@@ -0,0 +1,15 @@
+let usuario = document.getElementById("nombres").value
+let password = document.getElementById("nombres").value
+
+let users = {
+    usuario: "123",
+    password: "123"
+}
+
+
+function login() {
+    if (usuario.includes(users.usuario) && password.includes(users.password)) {
+        
+
+    }
+}
